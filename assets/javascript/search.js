1fbf398c1a3be53734649d80279732804c3415cd
Kaviz101586 <jason.mapou@gmail.com>
2018-12-08 14:10:25 -0500

    working

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -7,13 +7,14 @@ $("#resetBtn").on("click", function(){
  });
 
  $("#searchBtn").on("click", function() {
+     console.log("hello")
     var q = $("#searchTerm").val();
     var beginDate = $("#startYear").val();
     var endDate = $("#endYear").val();
     var queryNum = $("#numberOfRecords").val();
  
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
+    queryURL += '?' + $.param({
       'api-key': "d4fe7d5f1a9d4bb9bf15fcba0dd5f5f4",
       'q': q,
       'begin_date': beginDate + "0101",
